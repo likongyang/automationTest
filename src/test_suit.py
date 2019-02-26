5cdc8fbad284b058ebc990a31b49c0cc314e5045
Max Li <likongyang18@gmail.com>
2019-02-26 16:34:08 +0800

    unittest TestSuit

--- src/test_suit.py
+++ src/test_suit.py
@@ -5,9 +5,8 @@ from test_mathfunc import TestMathFunc
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestMathFunc))
 
-    tests = [TestMathFunc("test_add"), TestMathFunc("test_minus"), TestMathFunc("test_divide")]
-    suite.addTests(tests)
-
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+    with open('UnittestTextReport.txt', 'a') as f:
+        runner = unittest.TextTestRunner(stream=f, verbosity=2)
+        runner.run(suite)
